dd023ab7580d302220bb1a1747b76431ef60e528
Bhavana <bhavana.bhasker@gmail.com>
2015-11-22 23:24:44 -0800

    Use the Access Token Here

--- getStatus.go
+++ getStatus.go
@@ -24,7 +24,10 @@ func MakeRequest(requestid string) Makerequest {
 	var pm Makerequest
 	param := Url
 	param += requestid
-	accesstoken := "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOlsicmVxdWVzdCJdLCJzdWIiOiJiNTJjZjYxZC00ODA0LTQwNTAtYjRmNy02Yzk4ZDMyZmQ2NjgiLCJpc3MiOiJ1YmVyLXVzMSIsImp0aSI6IjQ1OGY3MTExLWIxNDItNGE5Ny04YWQyLWQ3NDQ1MWM4YTI5ZCIsImV4cCI6MTQ0OTgyMzY2OSwiaWF0IjoxNDQ3MjMxNjY4LCJ1YWN0IjoiaHNsZEE3aXkycnRJNkxhOUNBa0tIUXIwSlo3TmllIiwibmJmIjoxNDQ3MjMxNTc4LCJhdWQiOiJ1eF9wbFNFYkhSSTMybG1XZ0tpR09KMVN6YjRWencwbyJ9.ZPHFFP8CGQEWkmY4pOwaz8pamsA6a7ePB0xWAUPfP5EKpawH8_S2V-4WWAmNVDQqtCtHUQ7afqlMD13QFPZXEAn3Ztc3L1xGY_LACHzz2ZE8we5xktVgVrfzy7fFDCnwIHAkigmzoiQnaW5DW4_Sxjtf8Fz9gwqShNYCMDGWmx-TkbP6w425CHfyBXJObjJijpv_8Z6bXp-u8bYIAkNp0OitfRPfhZpErLiFbL01R7RzdLW1BkeiHC7Dw9513xwY7GvoZBkNdVU9WU2dlfT9k-CH0vakWEqJOSp7oyTpmNmsAmqtpENH1ASUjYKK5hswfhjVdxQ91LPCJevFxb1pBA"
+        // Valid Access token generated on November 10th . Expires December 10th
+	//Access Token needs to be filled here , For security reasons the access
+	//token has been emailed to the Professor	
+	accesstoken := ""
 	authstr := fmt.Sprintf("Bearer %s", accesstoken)
 	request, err := http.NewRequest("GET", param, nil)
 	if err != nil {
